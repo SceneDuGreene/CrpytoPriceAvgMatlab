9ca363c7fb236e28f1841ca772fe7aa85d9895cf
momosito <mike.3312@hotmail.com>
2021-10-28 12:26:27 -0700

    Update priceavg_array.m

--- priceavg_array.m
+++ priceavg_array.m
@@ -1,8 +1,18 @@
 % This program is used to figure out how many shares to buy of a stock to 
 % average the price to a desired amount based on initial investment
 
-info = {'BCH','0.07623','2623.63','641.1637','1300'};
 
+%% Place important information in 'info' variable
+
+% info(1) = Name of Asset
+% info(2) = Amount of Shares
+% info(3) = Cost of individual share
+% info(4) = Today's price of individual share
+% info(5) = Desired final averaged Price
+
+info = {'LSK','7.8','11.937','3.9448','5.0'};
+
+%% Calculations
 %Name of share
 name_share = char(info(1));
 %Already Purchased
@@ -18,7 +28,7 @@ desired_avg_price = str2double(info(5)); % desired final avg'd price ($)
 shares_to_buy = (total_cost - (total_units  * desired_avg_price)) / (desired_avg_price - price_current);
 amount_to_spend = shares_to_buy * price_current;
 
-fprintf('You need to buy %4.2f amount of %s. Totaling $%4.2f \n to bring down the price from %4.2f to %4.2f \n ', shares_to_buy, name_share ,  amount_to_spend, price_purchased, desired_avg_price);
+fprintf('You need to buy %4.2f amount of %s. Totaling $%4.2f \n to bring down the average price from %4.2f to %4.2f \n ', shares_to_buy, name_share ,  amount_to_spend, price_purchased, desired_avg_price);
  
 
 
